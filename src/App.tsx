363a15de3b1d9daebf89c7da2ae34e001ade3089
VanoKi <kirsvano@gmail.com>
2025-04-17 19:40:11 +0300

    feat: add mark up and styles

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,17 @@ function App() {
   const [] = useState()
 
   return (
-    <div></div>
+    <>
+      <div className={'counter'}>
+        <div className={'counter-display'}>
+          0
+        </div>
+        <div className={'button-panel'}>
+          <button className={'counter-button'}>inc</button>
+          <button className={'counter-button'}>reset</button>
+        </div>
+      </div>
+    </>
   )
 }
 
