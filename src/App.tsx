408f33a1902f8297cb8b2c5c99f185de563ae0d6
VanoKi <kirsvano@gmail.com>
2025-06-10 19:15:39 +0300

    feat: reducer

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,20 @@ import { useState } from 'react'
 import './App.css'
 import {CountDisplay} from "./components/CountDisplay/CountDisplay.tsx";
 import {ButtonPanel} from "./components/ButtonPanel/ButtonPanel.tsx";
+import {useReducer} from "react";
 
 function App() {
-  const [a, setA] = useState(0)
+  // const [a, setA] = useState(0)
+  const initialState = {count: 0}
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case 'increment' : return {count: state.count + 1}
+      case 'decrement' : return {count: state.count - 1}
+      default: return state
+    }
+  }
+
+  const [state, dispatch] = useReducer(reducer, initialState)
 
   const onclickIncHandler = () => {
       setA(a + 1)
