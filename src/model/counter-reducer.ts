6cc70aac00b351b8331a98fdf56863e3c5fdfd4f
VanoKi <kirsvano@gmail.com>
2025-07-23 10:40:56 +0300

    fix: counterReducer

--- src/model/counter-reducer.ts
+++ src/model/counter-reducer.ts
@@ -2,12 +2,9 @@ const initialState = 0
 type Actions = {type: 'inc'} | {type: 'dec'} | {type: 'res'}
 export const counterReducer = (state: number = initialState, action: Actions) => {
   switch (action.type) {
-    case 'inc': return state += 1
-      break
-    case "dec": return state -= 1
-      break
+    case 'inc': return state + 1
+    case "dec": return state - 1
     case 'res': return state = 0
-      break
     default: return state
   }
 }
