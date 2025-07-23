691599720d2c2b2caa9ad52fd7358fcbaf93aba0
VanoKi <kirsvano@gmail.com>
2025-07-23 11:56:42 +0300

    fix: CountDisplay

--- src/components/CountDisplay/CountDisplay.tsx
+++ src/components/CountDisplay/CountDisplay.tsx
@@ -1,13 +1,4 @@
 type Props = {
-<<<<<<< HEAD
-    count: number
-}
-
-export const CountDisplay = ({count}: Props) => {
-    return (
-        <div className={'counter-display'}>
-            <span className={count >= 5 ? 'red' : 'black'}>{count}</span>
-=======
     counter: number
 }
 
@@ -15,7 +6,6 @@ export const CountDisplay = ({counter}: Props) => {
     return (
         <div className={'counter-display'}>
             <span className={counter >= 5 ? 'red' : 'black'}>{counter}</span>
->>>>>>> ver2
         </div>
     )
 }
